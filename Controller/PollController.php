cb066ec8a7784e389ad6e2840550ae24b1dfd870
abienvenu <abi.github@abienvenu.net>
2014-09-04 01:01:27 +0200

    removed useless import

--- Controller/PollController.php
+++ Controller/PollController.php
@@ -22,7 +22,6 @@
 namespace Abienvenu\KyelaBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
