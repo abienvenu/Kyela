fa350f328b00d95dc6582d4624df7ae0ceb7aedb
Arnaud Bienvenu <abi@abienvenu.net>
2014-09-19 19:32:58 +0200

    Poll Controller Test

--- Tests/Controller/PollControllerTest.php
+++ Tests/Controller/PollControllerTest.php
@@ -6,50 +6,89 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class PollControllerTest extends WebTestCase
 {
-    /*
+	protected static $client;
+	protected static $translator;
+
+	public static function setUpBeforeClass()
+	{
+        // Create a new client to browse the application
+		self::$client = static::createClient();
+		self::$translator = self::$client->getContainer()->get('translator');
+	}
+
+	/**
+	 * Creates a poll
+	 *
+	 * @param string $name
+	 */
+	public static function createEntry($title)
+	{
+        $baseUrl = '/kyela/';
+        $crawler = self::$client->request('GET', $baseUrl);
+
+        $form = $crawler->selectButton(
+        	self::$translator->trans('create'))
+        	->form(['abienvenu_kyelabundle_newpoll[title]'  => $title]);
+        self::$client->submit($form);
+        return self::$client->followRedirect();
+	}
+
+	/**
+	 * Deletes a poll
+	 *
+	 * @param Crawler $crawler
+	 */
+	public static function deleteEntry($crawler)
+	{
+        $link = $crawler->selectLink(self::$translator->trans('edit.poll'));
+        $crawler = self::$client->click($link->link());
+        self::$client->submit($crawler->selectButton(self::$translator->trans('delete'))->form());
+        return self::$client->followRedirect();
+	}
+
+	/**
+	 * Edits a poll
+	 *
+	 * @param Crawler $crawler
+	 */
+	public static function editEntry($crawler, $newtitle)
+	{
+        $link = $crawler->selectLink(self::$translator->trans('edit.poll'));
+        $crawler = self::$client->click($link->link());
+        $form = $crawler->selectButton(
+        	self::$translator->trans('save'))
+        	->form(['abienvenu_kyelabundle_poll[title]' => $newtitle]);
+        self::$client->submit($form);
+        return self::$client->followRedirect();
+	}
+
     public function testCompleteScenario()
     {
-        // Create a new client to browse the application
-        $client = static::createClient();
+    	// Create entry
+        $title = uniqid('Test Poll ');
+        $crawler = self::createEntry($title);
 
-        // Create a new entry in the database
-        $crawler = $client->request('GET', '/poll/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /poll/");
-        $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
+        // Check it was created
+        $filter = 'h3.panel-title:contains("' . self::$translator->trans('success') . '")';
+        $message = $crawler->filter($filter);
+        $this->assertEquals(1, $message->count(), "Missing element $filter");
+        $filter = 'a:contains("' . $title . '")';
+        $linkToPoll = $crawler->filter($filter);
+        $this->assertEquals(1, $linkToPoll->count(), "Missing element $filter");
 
-        // Fill in the form and submit it
-        $form = $crawler->selectButton('Create')->form(array(
-            'abienvenu_kyelabundle_poll[field_name]'  => 'Test',
-            // ... other fields to fill
-        ));
+        // Edit entry
+        $newtitle = "$title 2";
+        $crawler = self::editEntry($crawler, $newtitle);
+        $filter = 'a:contains("' . $newtitle . '")';
+        $linkToPoll = $crawler->filter($filter);
+        $this->assertEquals(1, $linkToPoll->count(), "Missing element $filter");
 
-        $client->submit($form);
-        $crawler = $client->followRedirect();
+        // Delete entry
+        $crawler = self::deleteEntry($crawler);
 
-        // Check data in the show view
-        $this->assertGreaterThan(0, $crawler->filter('td:contains("Test")')->count(), 'Missing element td:contains("Test")');
-
-        // Edit the entity
-        $crawler = $client->click($crawler->selectLink('Edit')->link());
-
-        $form = $crawler->selectButton('Update')->form(array(
-            'abienvenu_kyelabundle_poll[field_name]'  => 'Foo',
-            // ... other fields to fill
-        ));
-
-        $client->submit($form);
-        $crawler = $client->followRedirect();
-
-        // Check the element contains an attribute with value equals "Foo"
-        $this->assertGreaterThan(0, $crawler->filter('[value="Foo"]')->count(), 'Missing element [value="Foo"]');
-
-        // Delete the entity
-        $client->submit($crawler->selectButton('Delete')->form());
-        $crawler = $client->followRedirect();
-
-        // Check the entity has been delete on the list
-        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
+        // Check it was deleted
+        $filter = 'div.panel-body:contains("' . self::$translator->trans('deleted') . '")';
+        $message = $crawler->filter($filter);
+        $this->assertEquals(1, $message->count(), "Missing element $filter");
     }
-
-    */
 }
