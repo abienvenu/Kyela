6db5fec1e5bd5158b00c863596487b78960525be
Arnaud Bienvenu <abi@abienvenu.net>
2014-09-20 20:25:12 +0200

    fix Choice Controller Test

--- Tests/Controller/ChoiceControllerTest.php
+++ Tests/Controller/ChoiceControllerTest.php
@@ -17,9 +17,7 @@ class ChoiceControllerTest extends PollWebTestCase
     	$name = uniqid('Test Choice ');
         $crawler = self::clickLink($crawler, 'add.an.option');
         self::checkElement($crawler, 'h1:contains("' . self::$translator->trans('new.choice') . '")');
-        $crawler = self::submitForm($crawler, 'create', 'choice', ['name' => $name, 'value' => 1, 'color' => 'green']);
-
-        return;
+        $crawler = self::submitForm($crawler, 'create', 'choice', ['name' => $name, 'value' => 1, 'color' => 'green', 'priority' => 12]);
 
         // Check data in the show view
         $filter = 'tr:contains("' . $name . '")';
@@ -28,7 +26,7 @@ class ChoiceControllerTest extends PollWebTestCase
         // Edit the choice
         $crawler = self::clickLink($crawler, 'edit');
         $name = "M $name";
-        $crawler = self::submitForm($crawler, 'save', 'choice', ['name' => $name, 'value' => 1, 'color' => 'green']);
+        $crawler = self::submitForm($crawler, 'save', 'choice', ['name' => $name, 'value' => 1, 'color' => 'green', 'priority' => 12]);
 
         // Check data in the show view
         $filter = 'tr:contains("' . $name . '")';
@@ -40,6 +38,9 @@ class ChoiceControllerTest extends PollWebTestCase
 
         // Check the choice has been deleted from the list
         $this->assertNotRegExp("/$name/", self::$client->getResponse()->getContent());
+
+        // Go back to the poll
+        $crawler = self::clickLink($crawler, 'back');
 
         // Delete the poll
         self::deletePollEntry($crawler);
