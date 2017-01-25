ca6241a6fc71a19e3fa369d26873222aee22e4a1
Arnaud Bienvenu <abi@abienvenu.net>
2017-01-25 23:51:03 +0100

    implemented ajax callback with data + elt

--- Resources/public/js/kyela.js
+++ Resources/public/js/kyela.js
@@ -2,16 +2,17 @@ Kyela = {
 	init: function()
 	{
 		$("button.participation").click(function (event) {
-			$(this).parent().parent().parent().children("button").hide();
-			$(this).parent().parent().parent().children("img.ajaxloader").show();
-			$.get($(this).data("url"), null, Kyela.onParticipationUpdateResponse($(this).parent().parent().parent()));
+			var elt = $(this).parent().parent().parent();
+			elt.children("button").hide();
+			elt.children("img.ajaxloader").show();
+			$.get($(this).data("url"), null, function(data) { Kyela.onParticipationUpdateResponse(data, elt) });
 		});
 	},
-	onParticipationUpdateResponse: function(elt)
+	onParticipationUpdateResponse: function(data, elt)
 	{
 		elt.children("img.ajaxloader").hide();
 		elt.children("button").show();
-		console.log("Done");
+		console.log("Done, got:" + data.color);
 	}
 }
 
