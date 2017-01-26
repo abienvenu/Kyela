e82106c97dd9b7cf0b7e9eb49e9a7b183d1343a5
Arnaud Bienvenu <abi@abienvenu.net>
2017-01-26 07:39:17 +0100

    code cleanup

--- Controller/ParticipationController.php
+++ Controller/ParticipationController.php
@@ -25,7 +25,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Abienvenu\KyelaBundle\Entity\Participation;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Participation controller.
