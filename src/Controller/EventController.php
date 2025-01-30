f07deb7231530cd84bb458626ab263ec597518f9
Arnaud Bienvenu <abi@abienvenu.net>
2025-01-30 17:19:45 +0100

    Use Param Converter for poll

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Poll;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Event;
@@ -15,13 +15,14 @@ class EventController extends AbstractController
 	{
 	}
 
-    /**
-     * Displays poll events
-     */
-	#[Route('/{pollUrl}/event/show')]
-    public function show(Request $request): Response
-    {
-        $events = $this->em->getRepository(Event::class)->getFutureEvents($request->get('poll'));
+	/**
+	 * Displays poll events
+	 */
+	#[Route('/{url:poll}/event/show')]
+	public function show(Poll $poll): Response
+	{
+		$events = $this->em->getRepository(Event::class)->getFutureEvents($poll);
+
 		return $this->render('event/show.html.twig', ['events' => $events]);
-    }
+	}
 }
--- src/Controller/PollController.php
+++ src/Controller/PollController.php
@@ -1,18 +1,20 @@
 <?php
+
 namespace App\Controller;
 
+use App\Entity\Poll;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PollController extends AbstractController
 {
-    /**
-     * Shows the poll
-     */
-	#[Route('/{pollUrl}/')]
-    public function show(): Response
-    {
-		return $this->render('poll/show.html.twig', []);
-    }
+	/**
+	 * Shows the poll
+	 */
+	#[Route('/{url:poll}/')]
+	public function show(Poll $poll): Response
+	{
+		return $this->render('poll/show.html.twig', ['poll' => $poll]);
+	}
 }
