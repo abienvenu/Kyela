65d3fa0fcf3b585dfa90b2721c64822984a88a0b
Arnaud Bienvenu <abi@abienvenu.net>
2025-02-02 15:54:57 +0100

    Line selector

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// Switch des icônes en haut à droite quand on plie/déplie
 document.querySelectorAll('.toggle-button').forEach(button => {
 	const icon = button.querySelector('i');
 	const target = document.querySelector(button.dataset.bsTarget);
@@ -22,5 +23,29 @@ document.querySelectorAll('.toggle-button').forEach(button => {
 
 	target.addEventListener('shown.bs.collapse', () => {
 		Object.entries(classes).forEach(([open, closed]) => toggleClass(closed, open));
+	});
+});
+
+// Sélection d'une ligne seule
+document.querySelectorAll('.participant').forEach(cell => {
+	cell.addEventListener('click', function () {
+		const row = cell.parentElement.parentElement;
+		const allRows = document.querySelectorAll('tbody tr');
+
+		// Si la ligne est déjà sélectionnée, on rétablit l'apparence par défaut
+		if (row.classList.contains('selected')) {
+			allRows.forEach(r => {
+				r.classList.remove('dimmed', 'selected');
+			});
+		} else {
+			// On ajoute l'effet dimmed sur toutes les lignes...
+			allRows.forEach(r => {
+				r.classList.add('dimmed');
+				r.classList.remove('selected');
+			});
+			// ...puis on retire l'effet sur la ligne cliquée et on la marque comme sélectionnée
+			row.classList.remove('dimmed');
+			row.classList.add('selected');
+		}
 	});
 });
