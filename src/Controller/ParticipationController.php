e76f99398e9bf4b4ba1bf984d4d51b49ac342aaf
Arnaud Bienvenu <abi@abienvenu.net>
2025-02-23 09:25:40 +0100

    Merge new+edit => update participation

--- src/Controller/ParticipationController.php
+++ src/Controller/ParticipationController.php
@@ -37,45 +37,26 @@ class ParticipationController extends AbstractController
 	}
 
 	/**
-	 * Create a new Participation on the fly
+	 * Create or update a new Participation on the fly
 	 */
-	#[Route('/participation/new/{event}/{participant}/{choice}')]
-	public function new(
+	#[Route('/participation/update/{event}/{participant}/{choice}')]
+	public function update(
 		Event $event,
 		Participant $participant,
 		Choice $choice,
 		EntityManagerInterface $em
 	): JsonResponse {
-		// If participation exists, editAction should have been called, not newAction
-		// But just in case, we look for an existing participation
 		$participation = $em
 			->getRepository(Participation::class)
 			->findOneBy(['participant' => $participant->getId(), 'event' => $event->getId()]);
 		if (!$participation) {
 			$participation = new Participation();
+			$em->persist($participation);
 		}
 		$participation->setEvent($event);
 		$participation->setParticipant($participant);
 		$participation->setChoice($choice);
 
-		$em->persist($participation);
-		$em->flush();
-
-		return new JsonResponse([
-			'success' => true,
-			'icon' => $choice->getIcon(),
-			'name' => $choice->getName(),
-			'color' => $choice->getColor(),
-		]);
-	}
-
-	/**
-	 * Edits a Participation on the fly
-	 */
-	#[Route('/participation/edit/{participation}/{choice}')]
-	public function edit(Participation $participation, Choice $choice, EntityManagerInterface $em): JsonResponse
-	{
-		$participation->setChoice($choice);
 		$em->flush();
 
 		return new JsonResponse([
