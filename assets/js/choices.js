6153f3628166deec29842e2789f6c3bac0f6ca09
Arnaud Bienvenu <abi@abienvenu.net>
2025-03-15 18:45:29 +0100

    Fix reorder choices on mobile

--- assets/js/choices.js
+++ assets/js/choices.js
@@ -1,24 +1,25 @@
 // Drag'n drop for choices
-document.addEventListener("DOMContentLoaded", function () {
-    let tbody = document.getElementById("choices-list");
+document.addEventListener('DOMContentLoaded', function () {
+	let tbody = document.getElementById('choices-list');
 
-    new Sortable(tbody, {
-        animation: 150,
-        ghostClass: "sortable-ghost",
-        onEnd: function (evt) {
-            let order = [];
-            document.querySelectorAll("#choices-list tr").forEach((row, index) => {
-                order.push({ id: row.dataset.id, priority: index + 1 });
-            });
+	new Sortable(tbody, {
+		animation: 150,
+		ghostClass: 'sortable-ghost',
+		handle: '.handle',
+		onEnd: function (evt) {
+			let order = [];
+			document.querySelectorAll('.handle').forEach((row, index) => {
+				order.push({id: row.dataset.id, priority: index + 1});
+			});
 
-            fetch("/" + pollUrl + "/choice/reorder", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "X-Requested-With": "XMLHttpRequest",
-                },
-                body: JSON.stringify(order),
-            });
-        },
-    });
+			fetch('/' + pollUrl + '/choice/reorder', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					'X-Requested-With': 'XMLHttpRequest'
+				},
+				body: JSON.stringify(order)
+			});
+		}
+	});
 });
